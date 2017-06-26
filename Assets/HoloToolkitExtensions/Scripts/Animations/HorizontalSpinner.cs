1e07c7ad48fa49a2ef6d3dd12630499d05fd3569
Joost van Schaik <joostvanschaik@outlook.com>
2017-06-26 14:55:54 +0200

    Initial code checkin

---
+++ Assets/HoloToolkitExtensions/Scripts/Animations/HorizontalSpinner.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+namespace HoloToolkitExtensions.Animation
+{
+    public class HorizontalSpinner: MonoBehaviour
+    {
+        public float SpinTime = 2.5f;
+
+        public bool AbsoluteAxis = false;
+
+        private void Start()
+        {
+            if (AbsoluteAxis)
+            {
+                LeanTween.rotateAround(gameObject, Vector3.up, 360 / 3.0f, SpinTime / 3.0f).setLoopClamp();
+            }
+            else
+            {
+                var rotation = Quaternion.AngleAxis(360f / 3.0f, Vector3.up).eulerAngles;
+                LeanTween.rotateLocal(gameObject, rotation, SpinTime / 3.0f).setLoopClamp();
+            }
+        }
+
+        private void OnEnable()
+        {
+            LeanTween.resume(gameObject);
+        }
+
+        private void OnDisable()
+        {
+            LeanTween.pause(gameObject);
+        }
+    }
+}
